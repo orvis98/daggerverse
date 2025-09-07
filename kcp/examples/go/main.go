6a5481883a625b08b2dda9737bf715351db0e9b3
John Enarsson <johnenarsson@hotmail.com>
2025-09-07 13:10:29 +0200

    Rewrite kcp module with bootstrap support

--- kcp/examples/go/main.go
+++ kcp/examples/go/main.go
@@ -10,11 +10,14 @@ import (
 type Examples struct{}
 
 // Creates and outputs an example Workspace tree.
-func (m *Examples) PrintWorkspaceTree(ctx context.Context) (string, error) {
-	return dag.Kcp(dagger.KcpOpts{Image: "ghcr.io/kcp-dev/kcp:v0.27.0"}).
-		Kubectl(dagger.KcpKubectlOpts{Arch: "arm64"}).
-		WithExec([]string{"kubectl", "create", "workspace", "org-1"}).
-		WithExec([]string{"kubectl", "create", "workspace", "org-2"}).
+func (m *Examples) PrintWorkspaceTree(
+	ctx context.Context,
+	// +optional
+	// +defaultPath="/root"
+	workspaces *dagger.Directory,
+) (string, error) {
+	return dag.Kcp().
+		WithWorkspaces(dagger.KcpWithWorkspacesOpts{Root: workspaces, Arch: "arm64"}).
 		WithExec([]string{"kubectl", "ws", "tree"}).
 		Stdout(ctx)
 }
--- kcp/main.go
+++ kcp/main.go
@@ -7,67 +7,81 @@ import (
 	"dagger/kcp/internal/dagger"
 	"encoding/json"
 	"fmt"
+	"strings"
 	"time"
 
 	"k8s.io/kops/pkg/kubeconfig"
 )
 
 type Kcp struct {
-	// +private
-	Name string
-
-	// Returns the kcp-admin kubeconfig as a file.
-	Kubeconfig *dagger.File
-
-	// Returns the kcp server container as a service.
-	Server *dagger.Service
+	// The kcp version.
+	Version string
+	// The generated client CA certificate.
+	ClientCaCertificate *dagger.File
+	// The generated client CA private key.
+	ClientCaKey *dagger.File
+	// The generated client certificate for kcp-admin.
+	ClientCertificate *dagger.File
+	// The generated client certificate private key for kcp-admin.
+	ClientCertificateKey *dagger.File
 }
 
-type PKI struct {
-	CACertificate          *dagger.File
-	ServerCertificate      *dagger.File
-	ServerPrivateKey       *dagger.File
-	AdminClientCertificate *dagger.File
-	AdminPrivateKey        *dagger.File
-}
-
-func bootstrapPKI() *PKI {
+func New(
+	// The kcp version to use.
+	// +optional
+	// +default="0.28.1"
+	version string,
+	// The openssl image to use for generating certificates.
+	// +optional
+	// +default="alpine/openssl:latest"
+	opensslImage string,
+) *Kcp {
 	ctr := dag.Container().
-		From("alpine").
-		WithExec([]string{"apk", "add", "openssl"}).
+		From(opensslImage).
 		// Generate CA certificate
 		WithExec([]string{"openssl", "req", "-x509", "-noenc", "-days", "365", "-newkey", "rsa:2048",
 			"-subj", "/CN=kcp-ca", "-out", "ca.crt", "-keyout", "ca.key"}).
-		// Generate server certificate
-		WithExec([]string{"openssl", "req", "-noenc", "-days", "90", "-newkey", "rsa:2048",
-			"-subj", "/CN=kcp-server", "-addext", "subjectAltName=DNS:kcp", "-out=server.csr", "-keyout=server.key"}).
-		WithExec([]string{"openssl", "x509", "-req", "-in", "server.csr", "-copy_extensions", "copyall",
-			"-CA", "ca.crt", "-CAkey", "ca.key", "-CAcreateserial", "-out", "server.crt"}).
 		// Generate client certificate for kcp-admin
 		WithExec([]string{"openssl", "req", "-noenc", "-days", "30", "-newkey", "rsa:2048",
 			"-subj", "/O=system:kcp:admin/CN=kcp-admin",
 			"-out", "admin.csr", "-keyout", "admin.key"}).
 		WithExec([]string{"openssl", "x509", "-req", "-in", "admin.csr", "-copy_extensions", "copyall",
 			"-CA", "ca.crt", "-CAkey", "ca.key", "-CAcreateserial", "-out", "admin.crt"})
-	return &PKI{
-		CACertificate:          ctr.File("ca.crt"),
-		ServerCertificate:      ctr.File("server.crt"),
-		ServerPrivateKey:       ctr.File("server.key"),
-		AdminClientCertificate: ctr.File("admin.crt"),
-		AdminPrivateKey:        ctr.File("admin.key"),
+	return &Kcp{
+		Version:              version,
+		ClientCaCertificate:  ctr.File("ca.crt"),
+		ClientCaKey:          ctr.File("ca.key"),
+		ClientCertificate:    ctr.File("admin.crt"),
+		ClientCertificateKey: ctr.File("admin.key"),
 	}
 }
 
-func setupAdminKubeconfig(ctx context.Context, pki *PKI) (*dagger.File, error) {
-	caCert, err := pki.CACertificate.Contents(ctx)
+// Returns a kcp server container.
+func (m *Kcp) Server() *dagger.Container {
+	return dag.Container().
+		From(fmt.Sprintf("ghcr.io/kcp-dev/kcp:v%s", m.Version)).
+		WithDirectory(".kcp", dag.Directory(), dagger.ContainerWithDirectoryOpts{Owner: "65532:65532"}).
+		WithEnvVariable("CACHEBUST", time.Now().String()).
+		WithFile("ca.crt", m.ClientCaCertificate, dagger.ContainerWithFileOpts{Owner: "65532:65532"}).
+		WithEntrypoint([]string{"/kcp", "start", "--client-ca-file=ca.crt"}).
+		WithExposedPort(6443, dagger.ContainerWithExposedPortOpts{Description: "kcp api-server"})
+}
+
+// Returns the kcp-admin kubeconfig.
+func (m *Kcp) Config(ctx context.Context) (*dagger.File, error) {
+	svc, err := m.Server().AsService().Start(ctx)
 	if err != nil {
 		return nil, err
 	}
-	clientCert, _ := pki.AdminClientCertificate.Contents(ctx)
+	ep, err := svc.Endpoint(ctx, dagger.ServiceEndpointOpts{Scheme: "https", Port: 6443})
 	if err != nil {
 		return nil, err
 	}
-	clientKey, _ := pki.AdminPrivateKey.Contents(ctx)
+	crt, err := m.ClientCertificate.Contents(ctx)
+	if err != nil {
+		return nil, err
+	}
+	key, err := m.ClientCertificateKey.Contents(ctx)
 	if err != nil {
 		return nil, err
 	}
@@ -78,8 +92,7 @@ func setupAdminKubeconfig(ctx context.Context, pki *PKI) (*dagger.File, error) {
 			{
 				Name: "root",
 				Cluster: kubeconfig.KubectlCluster{
-					Server:                   "https://kcp:6443/clusters/root",
-					CertificateAuthorityData: []byte(caCert),
+					Server: fmt.Sprintf("%s/clusters/root", ep),
 				},
 			},
 		},
@@ -87,8 +100,8 @@ func setupAdminKubeconfig(ctx context.Context, pki *PKI) (*dagger.File, error) {
 			{
 				Name: "admin",
 				User: kubeconfig.KubectlUser{
-					ClientCertificateData: []byte(clientCert),
-					ClientKeyData:         []byte(clientKey),
+					ClientCertificateData: []byte(crt),
+					ClientKeyData:         []byte(key),
 				},
 			},
 		},
@@ -106,50 +119,20 @@ func setupAdminKubeconfig(ctx context.Context, pki *PKI) (*dagger.File, error) {
 	if err != nil {
 		return nil, err
 	}
-	return dag.File("admin.kubeconfig", string(cfg)), err
-}
-
-func New(
-	ctx context.Context,
-	// When specified a cache volume will be configured.
-	// +optional
-	name string,
-	// The kcp server container image.
-	// +optional
-	// +default="ghcr.io/kcp-dev/kcp:v0.28.1"
-	image string,
-) *Kcp {
-	pki := bootstrapPKI()
-	cfg, err := setupAdminKubeconfig(ctx, pki)
+	hack, err := dag.Container().
+		From("alpine").
+		WithExec([]string{"apk", "add", "jq", "curl"}).
+		WithExec([]string{"sh", "-c", `until [ "$(curl --insecure -s -o /dev/null -w "%{http_code}" ` + ep + `/healthz)" = "200" ]; do sleep 1; done;`}).
+		WithExec([]string{"jq", `.clusters[0].cluster["insecure-skip-tls-verify"] = true`}, dagger.ContainerWithExecOpts{Stdin: string(cfg)}).
+		Stdout(ctx)
 	if err != nil {
-		return nil
+		return nil, err
 	}
-	return &Kcp{
-		Kubeconfig: cfg,
-		Server: dag.Container().
-			From(image).
-			With(func(c *dagger.Container) *dagger.Container {
-				// When a name is set, the caller is responsible for health checking the server after restarts.
-				if name != "" {
-					c = c.WithMountedCache(".kcp", dag.CacheVolume("kcp_data_"+name), dagger.ContainerWithMountedCacheOpts{Owner: "65532:65532"})
-				} else {
-					c = c.WithDirectory(".kcp", dag.Directory(), dagger.ContainerWithDirectoryOpts{Owner: "65532:65532"}).
-						WithEnvVariable("CACHEBUST", time.Now().String())
-				}
-				return c
-			}).
-			WithFile("ca.crt", pki.CACertificate, dagger.ContainerWithFileOpts{Owner: "65532:65532"}).
-			WithFile("server.crt", pki.ServerCertificate, dagger.ContainerWithFileOpts{Owner: "65532:65532"}).
-			WithFile("server.key", pki.ServerPrivateKey, dagger.ContainerWithFileOpts{Owner: "65532:65532"}).
-			WithEntrypoint([]string{"/kcp", "start", "--client-ca-file=ca.crt",
-				"--tls-cert-file=server.crt", "--tls-private-key-file=server.key"}).
-			WithExposedPort(6443, dagger.ContainerWithExposedPortOpts{Description: "kcp api-server"}).
-			AsService(),
-	}
+	return dag.File("config", hack), nil
 }
 
-// Returns a container with the kcp kubectl plugins and a kcp server service binding.
-func (m *Kcp) Kubectl(
+// Returns a kcp client container with the kcp plugins and the kcp-admin kubeconfig.
+func (m *Kcp) Client(
 	ctx context.Context,
 	// The system platform (should always be "linux").
 	// +optional
@@ -160,20 +143,78 @@ func (m *Kcp) Kubectl(
 	// +default="amd64"
 	arch string,
 ) (*dagger.Container, error) {
-	binDir := dag.Container().
-		From("alpine/kubectl").
-		WithExec([]string{"wget", fmt.Sprintf("https://github.com/kcp-dev/kcp/releases/download/v0.28.1/kubectl-create-workspace-plugin_0.28.1_%s_%s.tar.gz", platform, arch)}).
-		WithExec([]string{"tar", "-xf", fmt.Sprintf("kubectl-create-workspace-plugin_0.28.1_%s_%s.tar.gz", platform, arch), "-C", "/usr/local/bin", "--strip-components=1"}).
-		WithExec([]string{"wget", fmt.Sprintf("https://github.com/kcp-dev/kcp/releases/download/v0.28.1/kubectl-kcp-plugin_0.28.1_%s_%s.tar.gz", platform, arch)}).
-		WithExec([]string{"tar", "-xf", fmt.Sprintf("kubectl-kcp-plugin_0.28.1_%s_%s.tar.gz", platform, arch), "-C", "/usr/local/bin", "--strip-components=1"}).
-		WithExec([]string{"wget", fmt.Sprintf("https://github.com/kcp-dev/kcp/releases/download/v0.28.1/kubectl-ws-plugin_0.28.1_%s_%s.tar.gz", platform, arch)}).
-		WithExec([]string{"tar", "-xf", fmt.Sprintf("kubectl-ws-plugin_0.28.1_%s_%s.tar.gz", platform, arch), "-C", "/usr/local/bin", "--strip-components=1"}).
-		Directory("/usr/local/bin")
+	cfg, err := m.Config(ctx)
+	if err != nil {
+		return nil, err
+	}
 	return dag.Container().
-		From("alpine/curl").
-		WithServiceBinding("kcp", m.Server).
-		WithExec([]string{"sh", "-c", `until [ "$(curl --insecure -s -o /dev/null -w "%{http_code}" https://kcp:6443/healthz)" = "200" ]; do sleep 1; done;`}).
-		WithDirectory("/usr/local/bin", binDir).
-		WithFile("/.kube/config", m.Kubeconfig).
+		From("alpine/kubectl:latest").
+		WithExec([]string{"wget", fmt.Sprintf("https://github.com/kcp-dev/kcp/releases/download/v%s/kubectl-create-workspace-plugin_%s_%s_%s.tar.gz", m.Version, m.Version, platform, arch)}).
+		WithExec([]string{"tar", "-xf", fmt.Sprintf("kubectl-create-workspace-plugin_%s_%s_%s.tar.gz", m.Version, platform, arch), "-C", "/usr/local/bin", "--strip-components=1"}).
+		WithExec([]string{"wget", fmt.Sprintf("https://github.com/kcp-dev/kcp/releases/download/v%s/kubectl-kcp-plugin_%s_%s_%s.tar.gz", m.Version, m.Version, platform, arch)}).
+		WithExec([]string{"tar", "-xf", fmt.Sprintf("kubectl-kcp-plugin_%s_%s_%s.tar.gz", m.Version, platform, arch), "-C", "/usr/local/bin", "--strip-components=1"}).
+		WithExec([]string{"wget", fmt.Sprintf("https://github.com/kcp-dev/kcp/releases/download/v%s/kubectl-ws-plugin_%s_%s_%s.tar.gz", m.Version, m.Version, platform, arch)}).
+		WithExec([]string{"tar", "-xf", fmt.Sprintf("kubectl-ws-plugin_%s_%s_%s.tar.gz", m.Version, platform, arch), "-C", "/usr/local/bin", "--strip-components=1"}).
+		WithFile("/.kube/config", cfg).
 		WithEnvVariable("KUBECONFIG", "/.kube/config"), nil
+}
+
+func setupWorkspace(
+	ctx context.Context,
+	ctr *dagger.Container,
+	path string,
+	workspace *dagger.Directory,
+) (*dagger.Container, error) {
+	ctr = ctr.WithExec([]string{"kubectl", "ws", fmt.Sprintf(":%s", path)}).
+		WithDirectory("/build", workspace).
+		WithWorkdir("/build")
+	ents, err := workspace.Entries(ctx)
+	if err != nil {
+		return nil, err
+	}
+	for _, ent := range ents {
+		if strings.HasSuffix(ent, ".yaml") || strings.HasSuffix(ent, ".yml") {
+			ctr = ctr.WithExec([]string{"kubectl", "apply", "-f", ent})
+		} else if strings.HasSuffix(ent, "/") {
+			cut := strings.TrimSuffix(ent, "/")
+			ctr = ctr.WithExec([]string{"kubectl", "create", "workspace", cut})
+			ws, err := setupWorkspace(ctx, ctr, fmt.Sprintf("%s:%s", path, cut), workspace.Directory(cut))
+			if err != nil {
+				return nil, err
+			}
+			ec, err := ws.ExitCode(ctx)
+			if err != nil {
+				return nil, err
+			}
+			ctr = ctr.WithExec([]string{"echo", fmt.Sprintf("configured %s with exit code %d", cut, ec)})
+		}
+	}
+	return ctr, nil
+}
+
+func (m *Kcp) WithWorkspaces(
+	ctx context.Context,
+	// The system platform (should always be "linux").
+	// +optional
+	// +default="linux"
+	platform string,
+	// The system architecture (should be "amd64" or "arm64")
+	// +optional
+	// +default="amd64"
+	arch string,
+	// The directory tree to bootstrap in kcp.
+	// +optional
+	root *dagger.Directory,
+) (*dagger.Container, error) {
+	ctr, err := m.Client(ctx, platform, arch)
+	if err != nil {
+		return nil, err
+	}
+	ctr = ctr.WithDirectory("/build", root).
+		WithWorkdir("/build")
+	ctr, err = setupWorkspace(ctx, ctr, "root", root)
+	if err != nil {
+		return nil, err
+	}
+	return ctr, nil
 }
